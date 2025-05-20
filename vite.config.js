0d7355d79c849e3db2348b321d833a814b916f83
Ucha Kobakhidze <uchakobakhidze@gmail.com>
2025-05-20 00:56:07 -0700

    preparing Render deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+    host: '0.0.0.0',
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.jsx'],
